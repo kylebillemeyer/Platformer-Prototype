d6a2283824c9c02ed38eceb8bb10e652cca9c0b2
Kyle Billemeyer <billemeyer.k@gmail.com>
2022-05-17 01:03:05 -0400

    migrate bounce pads

--- dynamic/platforms/BouncePad.gd
+++ dynamic/platforms/BouncePad.gd
@@ -1,29 +1,18 @@
 tool
 extends Node2D
 
-export(int) var unit_width = 1
+export(int) var qunits_x = 1 setget set_qunits_x
 export(float) var launch_height = 600
 
-var unit_width_extent = 25
-var collision_margin = 10
-var height = 5
-var area
-var collisionShape
 var launch_guard_enabled
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    area = get_node("Area2D")
+    init()
     
-    var shape = CollisionShape2D.new()
-    var rect = RectangleShape2D.new()
-    rect.set_extents(Vector2(unit_width * unit_width_extent - collision_margin, height))
-    shape.set_shape(rect)
-    area.add_child(shape)
-
 func _draw():
-    var extents = Vector2(unit_width_extent * unit_width, height)
-    draw_rect(Rect2(area.get_position() - extents, extents * 2), Color.red, true)
+    var extents = calc_extents()
+    draw_rect(Rect2(Vector2.ZERO, extents * 2), Color.red, true)
 
 func _on_Area2D_body_entered(body):
     if body.get_name() == "Player" && !launch_guard_enabled:
@@ -39,3 +28,16 @@ func on_floor_callback(body):
     # calculated from the players feet
     var launch_speed = sqrt(2 * (launch_height + Globals.player_extents.y) * Globals.gravity)
     body.velocity = Vector2(0, -launch_speed)
+    
+func set_qunits_x(value: int) -> void:
+    qunits_x = value
+    init()
+    
+func init():
+    var extents = calc_extents()
+    
+    $Area2D.set_position(extents)
+    $Area2D/CollisionShape2D.get_shape().set_extents(extents)
+    
+func calc_extents():
+    return Vector2(qunits_x, 1) * (Globals.quarter_grid_size / 2)
